bc6f2bad4bc74926536cb75e901ef2603bb00593
gregoiretrouvain <pro.gregoire.moty@gmail.com>
2021-02-24 16:30:14 +0100

    feat: ajout de la premiere entitée

---
+++ src/Entity/Nationality.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NationalityRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+/**
+ * @ORM\Entity(repositoryClass=NationalityRepository::class)
+ * @UniqueEntity("slug")
+ */
+class Nationality
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     */
+    private $slug;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function generateSlug(SluggerInterface $slugger)
+    {
+        if (!$this->slug || '-' === $this->slug) {
+            $this->slug = $slugger->slug($this)->lower();
+        }
+    }
+
+}
---
+++ src/EntityListener/NationalityEntityListener.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\EntityListener;
+
+
+use App\Entity\Nationality;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+
+class NationalityEntityListener
+{
+    private $slugger;
+
+    public function __construct(SluggerInterface $slugger){
+        $this->slugger = $slugger;
+    }
+
+    public function prePersist(Nationality $nationality, LifecycleEventArgs $event){
+        $nationality->generateSlug($this->slugger);
+    }
+
+    public function preUpdate(Nationality $nationality, LifecycleEventArgs $event){
+        $nationality->generateSlug($this->slugger);
+    }
+}
