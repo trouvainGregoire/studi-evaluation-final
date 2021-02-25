c8b884f6e497864f62a4b75947fb0da317fa80b0
gregoiretrouvain <pro.gregoire.moty@gmail.com>
2021-02-25 14:22:23 +0100

    feat: ajout des relation pour l'admin au niveau des planques

--- src/Controller/Admin/HidewayCrudController.php
+++ src/Controller/Admin/HidewayCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Hideway;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class HidewayCrudController extends AbstractCrudController
 {
@@ -12,14 +16,14 @@ class HidewayCrudController extends AbstractCrudController
         return Hideway::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('address'),
+            TextField::new('code'),
+            AssociationField::new('country')->setRequired(true),
         ];
     }
-    */
 }
