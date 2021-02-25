0d937140d776acb4a5abecc9a72243206ac34a94
gregoiretrouvain <pro.gregoire.moty@gmail.com>
2021-02-25 14:25:01 +0100

    feat: ajout des relation pour l'admin au niveau des contacts

--- src/Controller/Admin/ContactCrudController.php
+++ src/Controller/Admin/ContactCrudController.php
@@ -4,6 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\Contact;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ContactCrudController extends AbstractCrudController
 {
@@ -12,14 +17,16 @@ class ContactCrudController extends AbstractCrudController
         return Contact::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name'),
+            TextField::new('firstName'),
+            DateField::new('birthdate'),
+            AssociationField::new('nationality')->setRequired(true),
+            TextField::new('codeName'),
         ];
     }
-    */
 }
