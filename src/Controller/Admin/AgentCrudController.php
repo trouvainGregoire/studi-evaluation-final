e6b5b711e7733d1b435b6839c604bb8f95d605d2
gregoiretrouvain <pro.gregoire.moty@gmail.com>
2021-02-25 14:29:33 +0100

    feat: ajout des relation pour l'admin au niveau des agents

--- src/Controller/Admin/AgentCrudController.php
+++ src/Controller/Admin/AgentCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Agent;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class AgentCrudController extends AbstractCrudController
 {
@@ -12,14 +16,17 @@ class AgentCrudController extends AbstractCrudController
         return Agent::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name'),
+            TextField::new('firstName'),
+            DateField::new('birthdate'),
+            AssociationField::new('nationality')->setRequired(true),
+            AssociationField::new('specialities')->setRequired(true),
+            TextField::new('identificationCode'),
         ];
     }
-    */
 }
