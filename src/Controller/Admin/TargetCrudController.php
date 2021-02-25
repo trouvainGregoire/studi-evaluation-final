454c6a2ee5400bf513b4bee4ee79035d0dc063a1
gregoiretrouvain <pro.gregoire.moty@gmail.com>
2021-02-25 14:27:07 +0100

    feat: ajout des relation pour l'admin au niveau des cibles

--- src/Controller/Admin/TargetCrudController.php
+++ src/Controller/Admin/TargetCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Target;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class TargetCrudController extends AbstractCrudController
 {
@@ -12,14 +16,16 @@ class TargetCrudController extends AbstractCrudController
         return Target::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name'),
+            TextField::new('firstName'),
+            DateField::new('birthdate'),
+            AssociationField::new('nationality')->setRequired(true),
+            TextField::new('codeName'),
         ];
     }
-    */
 }
